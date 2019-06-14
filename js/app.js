7c0d33cea17c11197e47cc4b330a15a2332906bd
Vanilna <lesyalp@gmail.com>
2019-06-14 08:48:21 +0200

    First commit (code isn't mine - I just need to add additional functionality)

---
+++ js/app.js
@@ -0,0 +1,71 @@
+// Enemies our player must avoid
+var Enemy = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    this.position = { x : 0, y : this.randomY()};
+    this.speed = this.randomFromThree();
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.position.x += (this.speed * dt);
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.position.x, this.position.y);
+};
+
+Enemy.prototype.randomFromThree = function() {
+   var random = Math.floor(Math.random() + (3 - 1 + 1)) + 1;
+   return random;
+}
+
+Enemy.prototype.randomY = function() {
+   var yPositions = [ 124, 207, 290];
+   var random = this.randomFromThree();
+   return yPositions[random - 1];
+}
+
+var allEnemiesArray = function () {
+    var enemy,
+    enemyArray = [];
+    for ( i = 0; i < 3; i++) {
+        enemy = new Enemy;
+        enemyArray.push(enemy);
+    }
+    console.log(enemyArray);
+    return enemyArray;
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
